a326ed25236f9b6a02f16a3d601f95af980adf8c
조현성 <hyunsung@johyeonseong-ui-MacBookAir.local>
2023-02-09 11:56:30 +0900

    구구단 구현

---
+++ gugudan/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <AppHeader
+  v-bind:propsnum1="num1"
+  v-bind:propsnum2="num2"
+  v-bind:propsnum3="result"
+  v-on:pass="clear"/>
+</template>
+
+<script>
+import AppHeader from "./components/App-Header.vue";
+export default {
+  components:{
+    AppHeader
+  },
+  data : function(){
+    return{
+      num1:Math.ceil(Math.random()*10),
+      num2:Math.ceil(Math.random()*10),
+    }
+  },
+  computed : {
+    result : function(){
+      return this.num1 * this.num2
+    }
+  },
+  methods:{
+    clear:function(){
+      this.num1 = Math.ceil(Math.random()*10),
+      this.num2 = Math.ceil(Math.random()*10)
+    }
+  }
+ 
+}
+</script>
+
+<style>
+
+</style>
---
+++ gugudan/src/components/App-Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>랜덤한 2개의 숫자가 주어집니다.</h1>
+    <hr>
+    <h2> 곱한 값을 구해보세요! </h2>
+    <hr>
+    <h3>숫자1 : {{propsnum1}}</h3>
+    <h3>숫자2 : {{propsnum2}}</h3>
+    
+    <form v-on:submit.prevent="SubmitForm">
+        <input type="number" v-model="input">
+        <button type="submit">입력</button>
+    </form>
+    <h1>정답 여부 : {{result_html}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['propsnum1','propsnum2','propsnum3'],
+    data(){
+        return{
+            result_html:'',
+            input : ''
+        }
+    },
+    methods:{
+        SubmitForm:function(){
+            if(this.propsnum3 == this.input){
+                this.$emit("pass");
+                this.result_html="정답";
+                this.input='';
+            }
+            else{
+                this.input='';
+                this.result_html="오답";
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
